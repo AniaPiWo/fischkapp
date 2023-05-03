28de0a00c66cff427a5a20d2c1f3ea77019f5171
AniaPiWo <snowflake@wp.pl>
2023-05-03 13:15:41 +0200

    card switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Header } from './components/Header.jsx';
-import { NewCard } from './components/NewCard.jsx';
-import { Card } from './components/Card.jsx';
-import "./styles.css"
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Header } from "./components/Header.jsx";
+import { NewCard } from "./components/NewCard.jsx";
+import { Card } from "./components/Card.jsx";
+import "./styles.css";
 
 const App = () => {
+  const [showNewCard, setShowNewCard] = useState(false);
+  const [cards, setCards] = useState([]);
+
+  const handleAddCard = () => {
+    setShowNewCard(true);
+  };
+
+  const handleSaveCard = (front, back) => {
+    const newCard = { front, back };
+    setCards([...cards, newCard]);
+    setShowNewCard(false);
+    console.log(cards);
+  };
+
   return (
     <div className="app">
-      <Header />
-      <Card />
+      <Header onAddCard={handleAddCard} />
+      {showNewCard ? ( <NewCard
+      onSaveCard={handleSaveCard}
+        frontValue={cards.length > 0 ? cards[cards.length - 1].front : ""}
+  />
+) : null}
+
+      <div className="cards-list">
+        {cards.map((card, index) => (
+          <Card key={index}  front={card.front} back={card.back} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
-
-
-
-/* //import { createState } from "./data/createState.js";
-import { createCardComponent } from "./ui/card.js";
-import { addCard } from "./data/actions.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  // Example of how we can create app state responsible for holding data
-  let appState = createState();
-
-  // Example of how we can create UI component using reusable function
-  const newCardData = { front: "Good morning", back: "Dzień dobry" };
-  const card = createCardComponent(newCardData);
-
-  // Example of how we can add card to our state
-  const updatedAppState = addCard(appState, newCardData);
-  appState = updatedAppState;
-
-  const app = document.getElementById("app");
-
-  // Example of how to display created card in our UI
-  app.append(card);
-
-  console.log(`You have ${appState.flashcards.length} card/s.`);
-});
- */
