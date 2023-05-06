8c09c9f444802bc87bf80d0e6a0b9ce691804256
AniaPiWo <snowflake@wp.pl>
2023-05-06 17:46:08 +0200

    edit

--- src/components/NewCard.jsx
+++ src/components/NewCard.jsx
@@ -1,74 +1,92 @@
 import React, { useState } from "react";
 import "../styles.css";
 import { TrashIcon } from "./TrashIcon";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export const NewCard = ({ onSaveCard, frontValue, deleteCard  }) => {
-  const [showBack, setShowBack] = useState(false);
-  const [front, setFront] = useState(frontValue || "");
+export const NewCard = ({ onSaveCard, deleteCard, id }) => {
+  const [front, setFront] = useState("");
+  const [back, setBack] = useState("");
+  const [isBack, setIsBack] = useState(false);
 
-  const handleNextClick = () => {
-    setShowBack(true);
-  };
-
-  const handleBackClick = () => {
-    setShowBack(false);
-  };
-
-  const handleSaveClick = () => {
-    onSaveCard(front, document.getElementById("set-back").value);
-  };
-
-  const handleFrontChange = (event) => {
-    setFront(event.target.value);
+  const handleSave = (e) => {
+    e.preventDefault();
+    if (!front || !back) {
+      toast("Please fill out both sides before saving!",{
+        position:"top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+      return;
+    }
+    onSaveCard(front, back);
   };
   
-  const handleDeleteCard = () => {
-    deleteCard();
-  };
-
 
   return (
     <div>
-      <div
-        id="new-card-front"
-        className="card"
-        style={{ display: showBack ? "none" : "block" }}
-      >
-        <form>
+      <ToastContainer 
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"/>
+      {!isBack ? (
+      <div id="new-card-front" className="card">
+        <form required>
           <input
             id="set-front"
             value={front}
-            onChange={handleFrontChange}
+            onChange={(e) => setFront(e.target.value)}
+            required
           />
         </form>
         <div className="button-box">
-          <button className="btn-1">Cancel</button>
-          <button className="btn-2" onClick={handleNextClick}>
+          <button className="btn-1" onClick={() => deleteCard(id)}>
+            Cancel
+          </button>
+          <button className="btn-2" onClick={() => setIsBack(true)}>
             Next
           </button>
         </div>
-      </div>
+      </div>) : (
       <div
         id="new-card-back"
         className="card"
-        style={{ display: showBack ? "block" : "none" }}
+        style={{ display: isBack ? "block" : "none" }}
       >
-        <div className="card-word"></div>
-        <div className="trash-icon" onClick={handleDeleteCard}>
+        <div className="card-word">{front}</div>
+        <div className="trash-icon" onClick={() => deleteCard(id)}>
           <TrashIcon />
         </div>
-        <form>
-          <input id="set-back" />
+        <form onSubmit={handleSave} required>
+          <input 
+            id="set-back" 
+            value={back} 
+            onChange={(e) => setBack(e.target.value)} 
+            required
+          />
         </form>
         <div className="button-box">
-          <button className="btn-1" onClick={handleBackClick}>
+          <button className="btn-1" onClick={() => setIsBack(false)}>
             Back
           </button>
-          <button className="btn-2" onClick={handleSaveClick}>
+          <button className="btn-2" onClick={handleSave}>
             Save
           </button>
         </div>
-      </div>
+      </div>)}
     </div>
   );
 };
+
